c1bf418aeb6bb2f64e14535851a06201ea8c732b
Charlotte Bouyer <bouyer.charlotte2@gmail.com>
2024-01-05 15:23:45 +0100

    ajout star /user

--- models/thing.js
+++ models/thing.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const thingSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  user: { type: String, required: true }
   
 });
 
